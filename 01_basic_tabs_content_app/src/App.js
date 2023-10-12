3f787b89dd3aa4a3c58498607c605a20498767dd
Khaled Allam <kaaoaa505@gmail.com>
2023-10-12 13:45:08 +0300

    Basic tabs content app

--- 01_basic_tabs_content_app/src/App.js
+++ 01_basic_tabs_content_app/src/App.js
@@ -1,23 +1,95 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./styles.css";
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ],
+  [
+    `Contrary to popular belief,
+     Lorem Ipsum is not simply random text. 
+     It has roots in a piece of classical Latin literature from 45 BC, 
+     making it over 2000 years old. 
+     Richard McClintock, 
+     a Latin professor at Hampden-Sydney College in Virginia`
+  ],
+];
 
 function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={logo} alt="React logo" />
+        <div>
+          <h1>React.js: Tabs App</h1>
+        </div>
       </header>
+
+      <div id="tabs">
+        <menu>
+          <button
+            className={activeContentIndex === 0 ? "active" : ""}
+            onClick={() => setActiveContentIndex(0)}
+          >
+            Why React?
+          </button>
+          <button
+            className={activeContentIndex === 1 ? "active" : ""}
+            onClick={() => setActiveContentIndex(1)}
+          >
+            Core Features
+          </button>
+          <button
+            className={activeContentIndex === 2 ? "active" : ""}
+            onClick={() => setActiveContentIndex(2)}
+          >
+            Related Resources
+          </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            Test 4
+          </button>
+          <button
+            className={activeContentIndex === 4 ? "active" : ""}
+            onClick={() => setActiveContentIndex(4)}
+          >
+            Test 5
+          </button>
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeContentIndex].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

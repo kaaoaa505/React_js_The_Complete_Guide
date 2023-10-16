66753df0d5e26bbe0b8268045982c291d6cebcc2
Khaled Allam <kaaoaa505@gmail.com>
2023-10-16 15:58:38 +0300

    Array slice, index, map, and push

--- 02_JavaScript_Refresher/assets/scripts/app.js
+++ 02_JavaScript_Refresher/assets/scripts/app.js
@@ -5,12 +5,36 @@ console.log(utils.default);
 console.log(utils.apiKey);
 console.log(utils.test);
 
-let userMessage = 'Hello world.';
-console.log(userMessage);
+let user_message = 'Hello world.';
+console.log(user_message);
 
-userMessage = 'new value.';
-console.log(userMessage);
+user_message = 'new value.';
+console.log(user_message);
 
 const user = new User('Khaled', 34);
 console.log(user);
 console.log(user.greet());
+
+const hobbies = ['sport', 'cooking', 'reading'];
+console.log(hobbies.slice());
+
+hobbies.push('working');
+console.log(hobbies);
+
+let hobby_index = hobbies.findIndex(item =>  item === 'reading');
+console.log(hobby_index);
+
+hobby_index = hobbies.findIndex(item =>  item === 'working');
+console.log(hobby_index);
+
+const new_hobbies = hobbies.map(hobby => {
+    return hobby.charAt(0).toUpperCase() + hobby.slice(1) + '!.';
+});
+console.log(hobbies);
+console.log(new_hobbies);
+
+const new_hobbies_obj = new_hobbies.map(hobby => {
+    // return {name: hobby};
+    return {hobby};
+});
+console.log(new_hobbies_obj);

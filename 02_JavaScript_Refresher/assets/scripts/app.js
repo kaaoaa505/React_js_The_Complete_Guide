af86cc95f7ea0dd64b251311f31cd781702e831b
Khaled Allam <kaaoaa505@gmail.com>
2023-10-16 09:38:48 +0300

    html script type module import and export js files

---
+++ 02_JavaScript_Refresher/assets/scripts/app.js
@@ -0,0 +1,5 @@
+import * as utils from './utils';
+
+console.log(utils.default);
+console.log(utils.apiKey);
+console.log(utils.test);
---
+++ 02_JavaScript_Refresher/assets/scripts/utils.js
@@ -0,0 +1,3 @@
+export default 'default data here...';
+export let apiKey = 'sadfasdfasdf2342342asdfsadf';
+export let test = 'test 3';

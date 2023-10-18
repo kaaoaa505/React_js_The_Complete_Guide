cb88f4fd998aea48c5fad4bb25640e2760bae9a5
Khaled Allam <kaaoaa505@gmail.com>
2023-10-18 10:25:01 +0300

    Card component with border shadow and radius Using props className and children

--- 03_react_web_app/src/components/Expenses/Expenses.js
+++ 03_react_web_app/src/components/Expenses/Expenses.js
@@ -1,13 +1,16 @@
 import './Expenses.css';
 
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import Card from '../Card/Card';
 
 function Expenses(props) {
     return (
         <div className="Expenses">
+            <Card className="bg-gray">
             {props.expenses.map((expense, index) => (
                 <ExpenseItem key={index} expense={expense} />
             ))}
+            </Card>
         </div>
     );
 }

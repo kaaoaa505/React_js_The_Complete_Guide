d1700889ff9632802d7a00302f9f99ca3f7cad7e
Khaled Allam <kaaoaa505@gmail.com>
2023-10-18 09:34:01 +0300

    App expenses map to expense item Expense item using expense date component Expense date format month, year, and day

---
+++ 03_react_web_app/src/components/ExpenseDate/ExpenseDate.js
@@ -0,0 +1,15 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props){
+    return (
+        <div className="ExpenseDate">
+            <div className='date'>
+                <div className='month'>{props.date.toLocaleString('default', { month: 'long' })}</div>
+                <div className='year'>{props.date.getFullYear()}</div>
+                <div className='day'>{props.date.toLocaleString('default', { day: '2-digit' })}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
--- 03_react_web_app/src/components/ExpenseItem/ExpenseItem.jsx
+++ 03_react_web_app/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,9 +1,16 @@
-function ExpenseItem(){
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
+import './ExpenseItem.css';
+function ExpenseItem(props) {
     return (
         <div className="ExpenseItem">
-            Expense Item Component
+            <ExpenseDate date={props.expense.date} />
+
+            <div className="description">
+                <h2>{props.expense.title}</h2>
+                <div className="price">${props.expense.amount}</div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;

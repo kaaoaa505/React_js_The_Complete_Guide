c88ed21aba2158c5a0a7c5a38000c42d1026da9a
Khaled Allam <kaaoaa505@gmail.com>
2023-10-24 13:39:09 +0300

    Invalid empty goal input with red border

--- 06_goals_app/src/App/Goals/GoalInput/GoalInput.tsx
+++ 06_goals_app/src/App/Goals/GoalInput/GoalInput.tsx
@@ -5,22 +5,33 @@ import { SetStateAction, useState } from 'react';
 import Button from '../../_components/Button/Button';
 
 const GoalInput = (props: any) => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [entered_value, $entered_value] = useState('');
+  const [is_valid_value, $is_valid_value] = useState(true);
 
   const goalInputChange = (event: { target: { value: SetStateAction<string>; }; }) => {
-    setEnteredValue(event.target.value);
+    $is_valid_value(true);
+    $entered_value(event.target.value);
   };
 
   const formSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+
+    if(entered_value.trim().length === 0){
+      $is_valid_value(false);
+      return;
+    }else{
+      $is_valid_value(true);
+      props.onAddGoal(entered_value);
+    }
   };
 
   return (
     <form className='GoalInput' onSubmit={formSubmit}>
       <div className="form-control">
-        <label> Goal</label>
-        <input type="text" onChange={goalInputChange} />
+        <label>Goal</label>
+        <input type="text" onChange={goalInputChange} style={{
+          border: is_valid_value ? '1px solid black' : '1px solid red'
+        }} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>

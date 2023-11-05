9ab8dc722c90b93d73e261cd849d2956ad193887
Khaled Allam <kaaoaa505@gmail.com>
2023-11-05 10:43:51 +0300

    Use react useRef to elements current DOM details

--- 08_user_age_app/src/App/Users/UserCreate/UserCreate.tsx
+++ 08_user_age_app/src/App/Users/UserCreate/UserCreate.tsx
@@ -1,11 +1,14 @@
 import Button from '../../_components/Button/Button';
 import './UserCreate.scss';
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const UserCreate = (props: any) => {
     const [username, $username] = useState('');
     const [age, $age] = useState(0);
+
+    let name_input_ref = useRef(null);
+    let age_input_ref = useRef(null);
 
     const usernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         $username(event.target.value);
@@ -17,6 +20,12 @@ const UserCreate = (props: any) => {
 
     const formSubmit = (event: React.FormEvent<HTMLDivElement>) => {
         event.preventDefault();
+
+        console.log(name_input_ref);
+        console.log(name_input_ref.current);
+        
+        console.log(age_input_ref);
+        console.log(age_input_ref.current);
 
         if(username.trim().length === 0 || age < 1){
             console.log('Invalid input.')
@@ -36,12 +45,12 @@ const UserCreate = (props: any) => {
             <form>
                 <div className='form-group'>
                     <label htmlFor="username">User Name:</label>
-                    <input id="username" type="text" value={username} onChange={usernameChange} placeholder='Name...' />
+                    <input id="username" type="text" value={username} onChange={usernameChange} placeholder='Name...' ref={name_input_ref} />
                 </div>
 
                 <div className='form-group'>
                     <label htmlFor="age">Age:</label>
-                    <input id="age" type="number" min={0} max={999} value={age === 0 ? '' : age} onChange={ageChange} placeholder='Age (years)...' />
+                    <input id="age" type="number" min={0} max={999} value={age === 0 ? '' : age} onChange={ageChange} placeholder='Age (years)...' ref={age_input_ref} />
                 </div>
 
                 <Button>Submit</Button>

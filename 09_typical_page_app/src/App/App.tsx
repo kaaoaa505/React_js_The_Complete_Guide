834f8da4e2bd4868f5e292bec1fb4ecaa088585c
Khaled Allam <kaaoaa505@gmail.com>
2024-01-29 14:16:27 +0300

    Using global app context provider

--- 09_typical_page_app/src/App/App.tsx
+++ 09_typical_page_app/src/App/App.tsx
@@ -1,47 +1,24 @@
 import './App.scss';
 
-import React, { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import MainHeader from './_partial/MainHeader/MainHeader';
 import Home from './Home/Home';
 import Login from './Login/Login';
-import AuthContext from '../store/AuthContext';
+import AuthContext, { AuthContextProvider } from '../store/AuthContext';
 
 function App() {
-  const [isLoggedIn, $isLoggedIn] = useState(false);
-
-  const storage_is_logged_in = localStorage.getItem('is_logged_in');
-
-  useEffect(() => {
-    if (storage_is_logged_in === 'true') {
-      $isLoggedIn(true);
-    }
-  }, [storage_is_logged_in]);
-
-  const loginHandler = (email: string, password: string) => {
-    $isLoggedIn(true);
-
-    localStorage.setItem('is_logged_in', 'true');
-  };
-
-  const logoutHandler = () => {
-    $isLoggedIn(false);
-
-    localStorage.removeItem('is_logged_in');
-  };
+  const ctx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider value={{
-      is_logged_in: isLoggedIn,
-      onLogin: loginHandler,
-      onLogout: logoutHandler,
-    }}>
+    <div className="App">
       <MainHeader />
+
       <main>
-        {!isLoggedIn && <Login />}
-        {isLoggedIn && <Home />}
+        {!ctx.is_logged_in && <Login />}
+        {ctx.is_logged_in && <Home />}
       </main>
-    </AuthContext.Provider>
+    </div>
   );
 }
 
--- 09_typical_page_app/src/store/AuthContext.tsx
+++ 09_typical_page_app/src/store/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function onLogin(email: string, password: string) { }
 function onLogout() { }
@@ -8,5 +8,35 @@ const AuthContext = React.createContext({
     onLogin,
     onLogout,
 });
+
+export const AuthContextProvider = (props: any) => {
+    const [isLoggedIn, $isLoggedIn] = useState(false);
+
+    const storage_is_logged_in = localStorage.getItem('is_logged_in');
+
+    useEffect(() => {
+        if (storage_is_logged_in === 'true') {
+            $isLoggedIn(true);
+        }
+    }, [storage_is_logged_in]);
+
+    const loginHandler = (email: string, password: string) => {
+        $isLoggedIn(true);
+
+        localStorage.setItem('is_logged_in', 'true');
+    };
+
+    const logoutHandler = () => {
+        $isLoggedIn(false);
+
+        localStorage.removeItem('is_logged_in');
+    };
+
+    return <AuthContext.Provider value={{
+        is_logged_in: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+    }}>{props.children}</AuthContext.Provider>
+};
 
 export default AuthContext;

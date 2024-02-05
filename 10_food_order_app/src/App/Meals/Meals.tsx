241b327b99c5861ac2898f6452eaf5bf73c19f4e
Khaled Allam <kaaoaa505@gmail.com>
2024-02-05 10:41:06 +0300

    Meals Summary

--- 10_food_order_app/src/App/Meals/Meals.tsx
+++ 10_food_order_app/src/App/Meals/Meals.tsx
@@ -1,10 +1,12 @@
 import './Meals.scss';
 
 import MealsAvailable from "./MealsAvailable/MealsAvailable";
+import MealsSummary from './MealsSummary/MealsSummary';
 
 const Meals = () => {
     return (
         <section className="Meals">
+            <MealsSummary />
             <MealsAvailable />
         </section>
     )

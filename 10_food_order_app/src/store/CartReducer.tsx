eff92bdb45eee1c76995729d9bfa77cb3b6976a4
Khaled Allam <kaaoaa505@gmail.com>
2024-03-18 13:56:14 +0300

    Disable duplicated request

--- 10_food_order_app/src/store/CartReducer.tsx
+++ 10_food_order_app/src/store/CartReducer.tsx
@@ -2,8 +2,16 @@ import CartInit from "./CartInit";
 
 const CartReducer = (state: any, action: any) => {
     if (action.type === 'ADD') {
-        const items = state.items.concat(action.item);
+        let items = state.items.slice();
         const total_amount = Number(state.total_amount) + (Number(action.item.price) * Number(action.item.amount));
+
+        const item_found_index = items.findIndex((item: any) => item.id === action.item.id);
+
+        if(item_found_index >= 0){
+            items[item_found_index]['amount'] += action.item.amount;
+        }else{
+            items = items.concat(action.item);
+        }
 
         return {
             items: items,

791e6169e46ff6ee78f9a7c982b858e529de3739
Khaled Allam <kaaoaa505@gmail.com>
2024-03-26 15:02:43 +0300

    Optimize list component reload with sorted array by using memo

--- 11_update_in_action_app/src/App/App.tsx
+++ 11_update_in_action_app/src/App/App.tsx
@@ -1,16 +1,18 @@
 import './App.scss';
 
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 
 import TextStaticMemo from './TextStaticMemo/TextStaticMemo';
 import TextData from './TextData/TextData';
 import Button from './UI/Button/Button';
+import ListData from './ListData/ListData';
 
 function App() {
   console.log('App is running...');
 
   const [show_txt, $show_txt] = useState(false);
   const [allow_show_txt, $allow_show_txt] = useState(false);
+  const [list_title, $title] = useState('My List');
 
   const showText = useCallback(() => {
     $show_txt(true);
@@ -25,13 +27,21 @@ function App() {
     $allow_show_txt(true);
   }, []);
 
+  const changeTitle = useCallback(() => {
+    $title('New Title');
+  }, []);
+
+  const items = useMemo(() => {
+    return [5, 3, 1, 10, 9]
+  }, []);
+
   return (
     <div className="App">
       <h1>Component reload partial DOM</h1>
       <TextStaticMemo />
 
       <hr />
-      
+
       <TextData />
 
       <hr />
@@ -39,6 +49,11 @@ function App() {
       {show_txt && <p>This is new dynamic text That reload App component</p>}
       {!show_txt && allow_show_txt && <Button onClick={showText}>Show text with app reload</Button>}
       {!allow_show_txt && <Button onClick={allowShowText}>Allow show text</Button>}
+
+      <hr />
+
+      <ListData title={list_title} items={items} />
+      <Button onClick={changeTitle}>Change List Title</Button>
     </div>
   );
 }
---
+++ 11_update_in_action_app/src/App/ListData/ListData.tsx
@@ -0,0 +1,27 @@
+import React, { useMemo } from 'react';
+
+import './ListData.scss';
+
+const ListData = (props: any) => {
+    console.log('ListData is running...');
+
+    const items = props.items;
+
+    const sortedList = useMemo(() => {
+        console.log('ListData items sorted.');
+        return items.sort((a: number, b: number) => a - b);
+    }, [items]);
+
+    return (
+        <div className='ListData'>
+            <h2>{props.title}</h2>
+            <ul>
+                {sortedList.map((item: any) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default React.memo(ListData);

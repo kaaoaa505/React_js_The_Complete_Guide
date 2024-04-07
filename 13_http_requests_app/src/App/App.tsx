1739a66ff128d1cf3da08026f55d3d36a91e7cff
Khaled Allam <kaaoaa505@gmail.com>
2024-04-07 11:27:45 +0300

    Load http json by fetch js get method

--- 13_http_requests_app/src/App/App.tsx
+++ 13_http_requests_app/src/App/App.tsx
@@ -3,15 +3,28 @@ import './App.scss';
 import Movies from './Movies/Movies';
 
 import MoviesExamples from '../Data/Examples/MoviesExamples';
+import { useState } from 'react';
 
 function App() {
+  let [movies, $movies] = useState(MoviesExamples);
+
+  const fetchMoveies = () => {
+    const result = fetch('https://swapi.dev/api/films');
+    result.then(async (response: any) => {
+      const data = await response.json();
+      
+      console.log(data);
+
+      $movies(data.results);
+    })
+  }
   return (
     <div className="App">
       <section>
-        <button>Load Movies</button>
+        <button onClick={fetchMoveies}>Load Movies</button>
       </section>
       <section>
-        <Movies movies={MoviesExamples} />
+        <Movies movies={movies} />
       </section>
     </div>
   );
--- 13_http_requests_app/src/App/Movies/Movies.tsx
+++ 13_http_requests_app/src/App/Movies/Movies.tsx
@@ -5,11 +5,13 @@ import Movie from './Movie/Movie';
 import MovieInterface from '../../Interfaces/MovieInterface';
 
 const Movies = (props: any) => {
+  let increment_index = 0;
+
   return (
     <ul className='Movies'>
       {props.movies.map((movie: MovieInterface) => (
         <Movie
-          key={movie.id}
+          key={increment_index++}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}

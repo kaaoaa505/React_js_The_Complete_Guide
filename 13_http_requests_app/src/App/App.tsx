eb429bee3f9d664d678b2aae9b0eb78bd7d69fbc
Khaled Allam <kaaoaa505@gmail.com>
2024-04-08 11:28:07 +0300

    Using content type and accept application json

--- 13_http_requests_app/src/App/App.tsx
+++ 13_http_requests_app/src/App/App.tsx
@@ -8,24 +8,35 @@ import { useState } from 'react';
 function App() {
   let [movies, $movies] = useState(MoviesExamples);
   let [is_loading, $is_loading] = useState(false);
+  let [error, $error] = useState(null);
 
   const fetchMoveies = async () => {
     $is_loading(true);
+    $error(null);
 
-    const response = await fetch('https://swapi.dev/api/films');
+    try {
+      const response = await fetch('https://swapi.dev/api/films', {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+      });
 
-    const json_data = await response.json();
+      const json_data = await response.json();
 
-    const results = json_data.results.map((movie: any) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        releaseDate: movie.release_date,
-        openingText: movie.opening_crawl,
-      }
-    });
+      const results = json_data.results.map((movie: any) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        }
+      });
 
-    $movies(results);
+      $movies(results);
+    } catch (new_error: any) {
+      $error(new_error.message);
+    }
 
     $is_loading(false);
   }
@@ -35,6 +46,9 @@ function App() {
       <section>
         <button onClick={fetchMoveies}>Load Movies</button>
       </section>
+
+      {error !== null && <section>{error}</section>}
+
       <section>
         {is_loading && <p>Loading....</p>}
         {!is_loading && movies.length === 0 && <p>No movie found.</p>}

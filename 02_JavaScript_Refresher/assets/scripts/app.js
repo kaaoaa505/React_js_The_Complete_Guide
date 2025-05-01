c5b3bfad55680a5562ab96cf3dcb5645f7c9524d
Khaled Allam <kaaoaa505@gmail.com>
2025-05-01 14:43:33 +0300

    using js exported methods in html type module

--- 02_JavaScript_Refresher/assets/scripts/app.js
+++ 02_JavaScript_Refresher/assets/scripts/app.js
@@ -5,3 +5,10 @@ import './dev/destructuring';
 import './dev/spreading';
 import './dev/dom';
 import './dev/functions';
+
+function doSomething(idx) {
+    console.log('Doing Some thing ...........................' + idx);
+  }
+  
+  // ✅ Manually expose to global scope
+  window.doSomething = doSomething;
